6475b5417580f6cae2f7f007d69a661b10f33859
NewLordVile <roman.mayr@gmx.net>
2020-04-11 20:16:44 +0200

    fix: made handling of the credentials more secure

--- src/main.py
+++ src/main.py
@@ -2,35 +2,41 @@ import json
 import os
 import sys
 import argparse
+from pathlib import Path
 from getpass import getpass
 
 
 import authentication
 import course_retrieval
 
+# Build path relative to absolute path of script (or rather the scripts parent dir)
+# Source: https://stackoverflow.com/a/55051039
+BASE_PATH = Path(__file__).parent
+CREDENTIALS_PATH = (BASE_PATH / "./credentials.json").resolve()
+CONFIG_PATH = (BASE_PATH / "./download_config.json").resolve()
 
-def setup_credentials():
-    credentials_path = os.path.join(sys.path[0], 'credentials.json')
-    if os.path.isfile(credentials_path):
-        with open(credentials_path, 'r') as f:
+
+def get_credentials():
+    username = None
+    password = None
+    if os.path.isfile(CREDENTIALS_PATH):
+        with open(CREDENTIALS_PATH, 'r') as f:
             config_data = json.load(f)
-            try:
-                os.environ['USERNAME'] = config_data['username']
-                os.environ['PASSWORD'] = config_data['password']
-            except KeyError:
-                print('Check credentials.json file')
-                exit(1)
-    else:
-        print('credentials.json file not found. Setting up new credentials.json file...')
+            username = config_data.get("username", None)
+            password = config_data.get("password", None)
+    if username is None:
+        username = input("Enter username or email (e.g. go42tum or example@tum.de): ")
         config_data = {
-            "username": input('Enter username or email (e.g. go42tum/example@tum.de)\n'),
-            "password": getpass('Enter password: '),
+            "username": username,
+            "password": None,
         }
-        os.environ['USERNAME'] = config_data['username']
-        os.environ['PASSWORD'] = config_data['password']
-        config_json = json.dumps(config_data)
-        with open(credentials_path, 'w') as f:
-            f.write(config_json)
+        print(f'Saving user name in {CREDENTIALS_PATH} ...')
+        with open(CREDENTIALS_PATH, 'w') as f:
+            json.dump(config_data, f)
+    if password is None:
+        password = getpass(f'Enter password for {username}: ')
+
+    return username, password
 
 
 def list_resources(args, session):
@@ -99,10 +105,10 @@ if __name__ == "__main__":
 
     args = arg_parser.parse_args()
 
-    setup_credentials()
+    username, password = get_credentials()
     session = authentication.start_session(
-        os.environ['USERNAME'],
-        os.environ['PASSWORD']
+        username,
+        password
     )
     if session is None:
         print('Could not start session.')
