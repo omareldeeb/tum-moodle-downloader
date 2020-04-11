948e504b616dfe5aea4cd544c3ee3dd565c207ad
NewLordVile <roman.mayr@gmx.net>
2020-04-11 17:14:15 +0200

    feat!: changed the command line interface & refactored the argument parsing

--- Readme.md
+++ Readme.md
@@ -13,29 +13,38 @@ Quick Start
 ---
 
 * run  
-`$ python3 src/main.py -d course file [path]`  
+`$ python3 src/main.py download course file [destination]`  
 to
-download `file` from the specified `course` into the optionally
-specified `path`
+download `file`(s) from a `course` into a `destination` path
+* run  
+`$ python3 src/main.py list [course]`
+to list available resources of the specified `course` or, if no course is specified, list available courses
+* run
+`$ python3 src/main.py -h`
+for general help
+* run
+`$ python3 src/main.py list -h`
+for help concerning the `list` command
+* run
+`$ python3 src/main.py download -h`
+for help concerning the `download` command
+* Note:
     * _Running for the first time will prompt user for TUM Moodle
     credentials and semester and store them in a credentials.json file_
-* run  
-`$ python3 src/main.py -l [course]`
-to either list available courses if `course` is not specified or to list all available resources of the specified
-`course`
+    * Use `python` instead of `python3` on Windows
 
 Examples
 ---
-* `$ python3 src/main.py -l 'Analysis'`
-will list all sources of the course `'Analysis'` available for download.
+* `$ python3 src/main.py list "Analysis für Informatik"` 
+will list all resources of the course `Analysis für Informatik` available for download.
 
-* `$ python3 src/main.py -c 'Analysis' -f 'Hausaufgabe 10' -p '~/Documents/Uni/WS19/Analysis/Hausaufgaben'`  
-will search the user's courses for `'Analysis'`
-and find the course 'Analysis für Informatik [MA0902]'. In this example, the script
-will search for `'Hausaufgabe 10'` and find the assignment 'Hausaufgabe 10 und Präsenzaufgaben der Woche'.
-The script will then navigate to the assignment's page and download the associated file: 'Blatt10.pdf', which
-will then be saved in the specified path `'~/Documents/Uni/WS19/Analysis/Hausaufgaben'`.
+* `$ python3 src/main.py download "Analysis für Informatik" "Hausaufgabe 10" "~/Documents/Uni/WS19/Analysis/Hausaufgaben"` 
+will search the user's courses for `Analysis für Informatik`
+and find a matching course (e.g. "Analysis für Informatik [MA0902]"). In this example, the script
+will search for `Hausaufgabe 10` and find the assignment "Hausaufgabe 10 und Präsenzaufgaben der Woche".
+The script will then navigate to the assignment's page and download the associated file: "Blatt10.pdf", which
+will then be saved in the specified path `~/Documents/Uni/WS19/Analysis/Hausaufgaben`.
 
-* `$ python3 src/main.py -d 'Analysis' 'Hausaufgabe' '~/Documents/Uni/WS19/Analysis/Hausaufgaben'`  
-similar to above, however, finds multiple files that match `'Hausaufgabe'` and downloads
+* `$ python3 src/main.py downlaod "Analysis für Informatik" "Hausaufgabe" "~/Documents/Uni/WS19/Analysis/Hausaufgaben"`  
+similar to above, however, finds multiple files that match `Hausaufgabe` and downloads
 them **all**.
--- src/main.py
+++ src/main.py
@@ -1,29 +1,12 @@
 import json
 import os
 import sys
+import argparse
 from getpass import getpass
+
+
 import authentication
 import course_retrieval
-
-
-def parse_args() -> dict:
-    args = sys.argv[1:]
-    if not args:
-        print('Usage: python3 main.py [-l [course] | -d course file [path]]')
-        exit(1)
-    if args[0] == '-l':
-        if len(args) > 1:
-            return {'mode': 'list', 'course': args[1]}
-        else:
-            return {'mode': 'list', 'course': None}
-    elif args[0] == '-d':
-        if len(args) > 2:
-            return {'mode': 'download', 'course': args[1], 'file': args[2], 'path': None}
-        if len(args) > 3:
-            return {'mode': 'download', 'course': args[1], 'file': args[2], 'path': args[3]}
-    else:
-        print('Usage: python3 main.py [-l | -d] [course] [file] [path]')
-        exit(1)
 
 
 def setup_credentials():
@@ -50,8 +33,72 @@ def setup_credentials():
             f.write(config_json)
 
 
+def list_resources(args, session):
+    course_name = args.course
+    if course_name == "*":
+        print('Listing available courses: ')
+        course_retrieval.list_courses(session)
+        exit(0)
+
+    course = course_retrieval.get_course(session, course_name)
+    if course_name is not None and course is None:
+        print('Could not find course: ' + course_name)
+        exit(1)
+    else:
+        course.list_all_resources()
+        exit(0)
+
+
+def download(args, session):
+    course_name = args.course
+    file_name = args.file
+    destination_path = args.destination
+    course = course_retrieval.get_course(session, course_name)
+
+    course.download_resource(file_name, destination_path)
+
+
 if __name__ == "__main__":
-    args = parse_args()
+    # Instantiate the argument parser
+    arg_parser = argparse.ArgumentParser()
+
+    # Add subparsers for the different available commands
+    subparsers = arg_parser.add_subparsers()
+
+    list_command_description = "list available resources of the specified 'course' " \
+                               "or, if no course is specified, list available courses"
+    list_parser = subparsers.add_parser("list",
+                                        description=list_command_description,
+                                        help=list_command_description)
+    list_parser.add_argument("course",
+                             type=str,
+                             nargs='?',
+                             default='*',
+                             help="name of the course of which the resources are to be listed")
+    # Set the function which is to be executed, if the 'list' command is provided
+    list_parser.set_defaults(func=list_resources)
+
+    download_command_description = "download 'file'(s) from a 'course' into a 'destination' path"
+    download_parser = subparsers.add_parser("download",
+                                            description=download_command_description,
+                                            help=download_command_description,
+                                            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    download_parser.add_argument('course',
+                                 type=str,
+                                 help="name of the course from which to download")
+    download_parser.add_argument('file',
+                                 type=str,
+                                 help="name pattern for the files which are to be downloaded")
+    download_parser.add_argument('destination',
+                                 type=str,
+                                 nargs='?',
+                                 default='./moodle_downloads/',
+                                 help="path at which the download(s) should be stored")
+    # Set the function which is to be executed, if the 'download' command is provided
+    download_parser.set_defaults(func=download)
+
+    args = arg_parser.parse_args()
+
     setup_credentials()
     session = authentication.start_session(
         os.environ['USERNAME'],
@@ -61,20 +108,5 @@ if __name__ == "__main__":
         print('Could not start session.')
         exit(1)
 
-    course_name = args['course']
-    course = course_retrieval.get_course(session, course_name)
-    if course_name is not None and course is None:
-        print('Could not find course: ' + course_name)
-        exit(1)
-    if args['mode'] == 'list':
-        if course_name is None:
-            print('Listing available courses: ')
-            course_retrieval.list_courses(session)
-            exit(0)
-        else:
-            course.list_all_resources()
-            exit(0)
-    elif args['mode'] == 'download':
-        file = args['file']
-        path = './' if args['path'] is None else args['path']
-        course.download_resource(file, path)
+    # Call the function which is set based on the command provided in the arguments (see above for details)
+    args.func(args, session)
