db651a9d2496bc919ce60752bf3144787db1cbb1
omareldeeb <omareldeeb00@live.com>
2023-11-11 13:04:51 +0100

    Move config files outside of source directory

--- src/globals.py
+++ src/globals.py
@@ -3,9 +3,9 @@ from pathlib import Path
 # Build paths relative to absolute path of script (or rather the scripts parent dir)
 # Source: https://stackoverflow.com/a/55051039
 BASE_PATH = Path(__file__).parent
-CREDENTIALS_PATH = (BASE_PATH / "./credentials.json").resolve()
-COURSE_CONFIG_PATH = (BASE_PATH / "./course_config.json").resolve()
-DOWNLOAD_CONFIG_PATH = (BASE_PATH / "./download_config.json").resolve()
+CREDENTIALS_PATH = (BASE_PATH / "../credentials.json").resolve()
+COURSE_CONFIG_PATH = (BASE_PATH / "../course_config.json").resolve()
+DOWNLOAD_CONFIG_PATH = (BASE_PATH / "../download_config.json").resolve()
 
 global_session = None
 
